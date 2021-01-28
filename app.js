d9f1caa0a290b40d1b2f2917f693fb0bdaec8a1f
DeletedC <CurtisWoodsArt@gmail.com>
2021-01-27 21:53:21 -0500

    Added code for notes C, D, E

--- app.js
+++ app.js
@@ -2,16 +2,29 @@ const beeper = new (window.AudioContext || window.webkitAudioContext);
 
 // Creating a sine wav and volume
 // Connecting the volume to the DOM
-const sine = beeper.createOscillator();
+const beepC = beeper.createOscillator();
+const beepD = beeper.createOscillator();
+const beepE = beeper.createOscillator();
 const volume = beeper.createGain();
 volume.connect(beeper.destination);
 
-// Setting the volume to 0
-volume.gain.value = 0;
 
-// Starting the sound, connecting to volume
-sine.start();
-sine.connect(volume);
+// Setting note frequencies
+beepC.frequency.value = 523.25;
+beepD.frequency.value = 587.33;
+beepE.frequency.value = 659.25;
+
+// Setting the volume to 0
+// volume.gain.value = 0;
+
+// Starting the sounds, connecting to volume
+beepC.start();
+beepD.start();
+beepE.start();
+
+// beepC.connect(volume);
+// beepD.connect(volume);
+// beepE.connect(volume);
 
 // Function to start and stop the sound
 // const beep = () => {
@@ -26,12 +39,37 @@ sine.connect(volume);
 //     }
 // }
 
-// Turns the volume up
-const beepStart = () => {
-    volume.gain.value = 1;
+// Connects a note
+const beepStart = (id) => {
+
+    switch (id) {
+        case 1:
+            beepC.connect(volume);
+            break;
+        case 2:
+            beepD.connect(volume);
+            break;
+        case 3:
+            beepE.connect(volume);
+            break;
+        default:
+            alert("SWITCH DEFAULT: Something wasn't caught in the connection.");
+    }
 }
 
-// Turns the volume down
-const beepStop = () => {
-    volume.gain.value = 0;
+// Disconnects a note
+const beepStop = (id) => {
+    switch (id) {
+        case 1:
+            beepC.disconnect();
+            break;
+        case 2:
+            beepD.disconnect();
+            break;
+        case 3:
+            beepE.disconnect();
+            break;
+        default:
+            alert("SWITCH DEFAULT: Something wasn't caught in the disconnection.");
+    }
 }
